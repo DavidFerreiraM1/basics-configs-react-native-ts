bfdd45459c90e5fb133d9cbbaef6c9a3143bdb9b
David Ferreira <42384312+DavidFerreiraM1@users.noreply.github.com>
2020-12-20 17:25:33 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -51,84 +51,50 @@ module.exports = {
     HeaderHeight: false,
   },
   rules: {
-    'global-require': 0,
-    'linebreak-style': [2, 'unix'],
-    'prefer-const': 0,
-    'no-console': [
-      'warn',
+  "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/jsx-filename-extension": [
+      1,
       {
-        allow: ['warn', 'error', 'log', 'info', 'disableYellowBox'],
-      },
+        "extensions": [".tsx"]
+      }
     ],
-    'no-param-reassign': ['error', {props: false}],
-    'no-restricted-globals': 0,
-    'no-unused-vars': 0,
-    'no-use-before-define': 0,
-    'no-underscore-dangle': 0,
-    'no-useless-constructor': 0,
-    'no-unused-expressions': 0,
-    'no-plusplus': 0,
-    'no-nested-ternary': 0,
-    'max-classes-per-file': 0,
-    'import/prefer-default-export': 0,
-    'react/prefer-stateless-function': 0,
-    'react/destructuring-assignment': 0,
-    'react/prop-types': 0,
-    'react/react-in-jsx-scope': 0,
-    'react/jsx-props-no-spreading': 0,
-    'react/jsx-filename-extension': [
-      0,
+    "import/prefer-default-export": "off",
+    "camelcase": "off",
+    "@typescript-eslint/ban-types": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
       {
-        extensions: ['.jsx', '.tsx'],
-      },
+        "ts": "never",
+        "tsx": "never"
+      }
     ],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
+    "indent": "off",
+    "react/jsx-indent": "off",
+    "react/jsx-indent-props": "off",
+    "prettier/prettier": "error",
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": "error",
+    "import/order": [
+      "error",
       {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-    'jsx-a11y/accessible-emoji': 0,
-    'react/static-property-placement': 0,
-    'react-native/no-color-literals': 0,
-    'react-native/no-raw-text': 0,
-    'react-native/no-inline-styles': 0,
-    'react-native/split-platform-components': 0,
-    '@typescript-eslint/explicit-member-accessibility': [
-      2,
-      {accessibility: 'no-public'},
-    ],
-    '@typescript-eslint/no-empty-interface': 1,
-    // '@typescript-eslint/explicit-function-return-type': [
-    //   0,
-    //   {
-    //     allowExpressions: true,
-    //     allowTypedFunctionExpressions: true,
-    //   },
-    // ],
-    '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/no-use-before-define': [
-      2,
-      {
-        functions: true,
-        classes: true,
-        variables: false,
-      },
-    ],
-    '@typescript-eslint/no-unused-vars': [1, {args: 'none'}],
-    '@typescript-eslint/no-non-null-assertion': 0,
-    '@typescript-eslint/camelcase': 0,
-    '@typescript-eslint/ban-ts-ignore': 0,
-    'react-native/sort-styles': [
-      'error',
-      'asc',
-      {ignoreClassNames: true, ignoreStyleProperties: true},
-    ],
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
+        "groups": ["builtin", "external", "internal"],
+        "pathGroups": [
+          {
+            "pattern": "react",
+            "group": "external",
+            "position": "before"
+          }
+        ],
+        "pathGroupsExcludedImportTypes": ["react"],
+        "newlines-between": "always",
+        "alphabetize": {
+          "order": "asc",
+          "caseInsensitive": true
+        }
+      }
+    ]
   },
   overrides: [
     {
